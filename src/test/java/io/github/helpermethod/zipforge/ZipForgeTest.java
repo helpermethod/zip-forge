4ee0788ba4c611726e2ef308804c44b039db0743
Oliver Weiler <wo6980@gmail.com>
2025-10-08 13:44:58 +0200

    chore: Switch to provideArguments(ParameterDeclarations parameters, ExtensionContext context) (#188)

--- src/test/java/io/github/helpermethod/zipforge/ZipForgeTest.java
+++ src/test/java/io/github/helpermethod/zipforge/ZipForgeTest.java
@@ -30,6 +30,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.support.ParameterDeclarations;
 
 @DisplayNameGeneration(ReplaceUnderscores.class)
 class ZipForgeTest {
@@ -76,7 +77,8 @@ class ZipForgeTest {
 
         static class ZipFileEntries implements ArgumentsProvider {
             @Override
-            public Stream<? extends Arguments> provideArguments(ParameterDeclarations parameters, ExtensionContext context) {
+            public Stream<? extends Arguments> provideArguments(
+                    ParameterDeclarations parameters, ExtensionContext context) {
                 return Stream.of(
                         arguments(
                                 (NodeGroup) () -> {
